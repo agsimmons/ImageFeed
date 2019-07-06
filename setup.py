65345d9126822ddc05582a47651d6024564b0bfd
Andrew Simmons <agsimmons0@gmail.com>
2019-07-06 13:03:07 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    # Package Metadata
+    name='ImageFeed',
+    version='1.0.0',
+    description='Expose a VideoCapture image sequence through a web server',
+    url='https://github.com/agsimmons/ImageFeed',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ],
+
+    packages=find_packages(),
+    data_files=[
+        ('', ['config.json']),
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'imagefeed = imagefeed.__main__:main'
+        ]
+    },
+
+    author='Andrew Simmons',
+    author_email='agsimmons0@gmail.com'
+)
