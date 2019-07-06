356b5b768288a3e89178b9624d6930d027536ee9
Andrew Simmons <agsimmons0@gmail.com>
2019-07-06 12:53:30 -0400

    Use absolute import

--- imagefeed/feed_manager.py
+++ imagefeed/feed_manager.py
@@ -1,13 +1,18 @@
+import sys
 import time
 
 import cv2
 
 
 def capture(refresh_interval, video_capture_index, temp_file):
+
+    cap = cv2.VideoCapture(video_capture_index)
+
+    if cap is None or not cap.isOpened():
+        print(f'ERROR: VideoCapture with index {video_capture_index} does not exist')
+        sys.exit(1)
+
     try:
-
-        cap = cv2.VideoCapture(video_capture_index)
-
         while True:
             ret, frame = cap.read()
             cv2.imwrite(temp_file, frame)
